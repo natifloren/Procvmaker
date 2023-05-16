cda20f09dd807a374c93d2d4104f6859bc2fde73
Leonardo Vitullo <leonardopcv@gmail.com>
2023-05-16 11:29:58 -0300

    swiper tunnig

--- Pagina para el proyecto final/css/swiper.js
+++ Pagina para el proyecto final/css/swiper.js
@@ -5,11 +5,15 @@ var swiper = new Swiper(".swiper", {
     centeredSlides: true,
     slidesPerView: "auto",
     coverflowEffect: {
-      rotate: 15,
+      rotate: 10,
       stretch: 0,
       depth: 300,
-      modifier: 1,
+      modifier: 2, //Tamaño de la imagen principal
       slideShadows: true,
     },
     loop: true,
+    autoplay: {
+        delay: 1000, // Tiempo de espera entre cada slide en milisegundos
+        disableOnInteraction: true, // Permite que el autoplay continúe incluso cuando el usuario interactúa con el Swiper
+      },
   });
